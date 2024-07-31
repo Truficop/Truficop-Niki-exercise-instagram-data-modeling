161b241975e9c3e5987fbd87843c6b6c479961b9
Nikita Malykhin <43642381+Truficop@users.noreply.github.com>
2024-07-31 11:25:52 +0000

    hecho

--- src/models.py
+++ src/models.py
@@ -1,29 +1,64 @@
 import os
 import sys
-from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy import Column, ForeignKey, Integer, String, Table
 from sqlalchemy.orm import relationship, declarative_base
 from sqlalchemy import create_engine
 from eralchemy2 import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
-    # Here we define columns for the table person
-    # Notice that each column is also a normal Python instance attribute.
-    id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
 
-class Address(Base):
-    __tablename__ = 'address'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
+
+follow = Table(
+    'follow',
+Base.metadata,
+Column('following_id',Integer,ForeignKey('User.id')),
+Column('follower_id',Integer,ForeignKey('User.id'))
+)
+
+class User(Base):
+    __tablename__ = 'User'
     id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+    username = Column(String(250), nullable=False, unique=True)
+    first_name = Column(String(250), nullable=False)
+    last_name = Column(String(250), nullable=False)
+    email = Column(String(250), nullable=False, unique=True)
+
+    follower = relationship('Follower', 
+                                secondary = follow, 
+                                primaryjoin = (follow.c.following_id == id),
+                                secondaryjoin = (follow.c.follower_id == id),
+                                backref = 'following', lazy=True)
+    
+    post = relationship('Post', backref='User', lazy=True)
+    comment = relationship('Comment', backref='User', lazy=True)
+
+# class Follower(Base):
+#     __tablename__ = 'Follower'
+#     user_from_id = Column(Integer, ForeignKey('User.id'), primary_key=True)
+#     user_to_id = Column(Integer, ForeignKey('User.id'), primary_key=True)
+
+class Post(Base):
+    __tablename__ = 'Post'
+    id = Column(Integer, primary_key=True)
+    user_id = Column(Integer, ForeignKey('User.id'))
+
+    media = relationship('Media', backref='Post', lazy=True)
+    comment = relationship('Comment', backref='Post', lazy=True)
+
+class Media(Base):
+    __tablename__ = 'Media'
+    id = Column(Integer, primary_key=True)
+    type = Column(String(250), nullable=False)
+    url = Column(String(250), nullable=False)
+    post_id = Column(Integer, ForeignKey('Post.id'))
+
+class Comment(Base):
+    __tablename__ = 'Comment'
+    id = Column(Integer, primary_key=True)  
+    comment_text = Column(String(250), nullable=False)
+    author_id = Column(Integer, ForeignKey('User.id'))
+    post_id = Column(Integer, ForeignKey('Post.id'))
 
     def to_dict(self):
         return {}
