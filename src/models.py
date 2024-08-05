0c461722c7eb03998eafa931d5afe57a88056387
Nikita Malykhin <43642381+Truficop@users.noreply.github.com>
2024-08-05 05:32:10 +0300

    Update models.py

--- src/models.py
+++ src/models.py
@@ -63,6 +63,75 @@ class Comment(Base):
     def to_dict(self):
         return {}
 
+
+
+# ================================== COPIA PARA EL MODELING DE BLOG DE STAR WARS ===========================================
+# ====================================================== INICIO ============================================================
+
+# class Usuario(Base):
+#     __tablename__ = 'usuario'
+#     id = Column(Integer, primary_key=True)
+#     email = Column(String(250),nullable=False, unique=True)
+#     password = Column(String(250),nullable=False)
+#     nombre = Column(String(250),nullable=False)
+#     apellido = Column(String(250),nullable=False)
+#     fecha_de_subscripcion = Column(DateTime, nullable=False)
+#     favoritos_planetas = relationship('FavoritoPlaneta', backref='usuario')
+#     favoritos_personajes = relationship('FavoritoPersonaje', backref='usuario')
+
+
+# class Planeta(Base):
+#     __tablename__ = 'planeta'
+#     id = Column(Integer, primary_key=True)
+#     nombre = Column(String(250), nullable=False)
+#     population = Column(Integer, nullable=False)
+#     rotation_period = Column(Integer, nullable=False)
+#     orbital_period = Column(Integer, nullable=False)
+#     diameter = Column(Integer, nullable=False)
+#     gravity = Column(String(250), nullable=False)
+#     terrain = Column(String(250), nullable=False)
+#     surface = Column(String(250), nullable=False)
+#     climate = Column(String(250), nullable=False)
+#     favoritos_planetas = relationship('FavoritoPlaneta', backref='planeta')
+
+
+# class Personaje(Base):
+#     __tablename__ = 'personaje'
+#     id = Column(Integer, primary_key=True)
+#     nombre = Column(String(250), nullable=False)
+#     birth_year = Column(Integer, nullable=False)
+#     species = Column(String(250), nullable=False)
+#     height = Column(Integer, nullable=False)
+#     mass = Column(Integer, nullable=False)
+#     gender = Column(String(250), nullable=False)
+#     hair_color = Column(String(250), nullable=False)
+#     skin_color = Column(String(250), nullable=False)
+#     homeworld = Column(String(250), ForeignKey('planeta.id'))
+#     favoritos_personajes = relationship('FavoritoPersonaje', backref='personaje')
+
+
+# class FavoritoPlaneta(Base):
+#     __tablename__ = 'favorito_planeta'
+#     id = Column(Integer, primary_key=True)
+#     usuario_id = Column(Integer, ForeignKey('usuario.id'))
+#     planeta_id = Column(Integer, ForeignKey('planeta.id'))
+#     fecha_de_favorito = Column(DateTime, nullable=False)
+#     usuario = relationship('Usuario', backref='favoritos_planetas')
+#     planeta = relationship('Planeta', backref='favoritos_planetas')
+
+
+# class FavoritoPersonaje(Base):
+#     __tablename__ = 'favorito_personaje'
+#     id = Column(Integer, primary_key=True)
+#     usuario_id = Column(Integer, ForeignKey('usuario.id'))
+#     personaje_id = Column(Integer, ForeignKey('personaje.id'))
+#     fecha_de_favorito = Column(DateTime, nullable=False)
+#     usuario = relationship('Usuario', backref='favoritos_personajes')
+#     personaje = relationship('Personaje', backref='favoritos_personajes')
+
+# ====================================================== FIN ============================================================
+# ================================== COPIA PARA EL MODELING DE BLOG DE STAR WARS ===========================================
+
 ## Draw from SQLAlchemy base
 try:
     result = render_er(Base, 'diagram.png')
